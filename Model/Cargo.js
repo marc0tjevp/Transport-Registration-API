9261d4e6a12f4d86cf89300ecde99e4b1be46ee4
Djim <oomesdjim@gmail.com>
2018-05-24 15:43:58 +0200

    Added basic functionality

---
+++ Model/Cargo.js
@@ -0,0 +1,34 @@
+class Cargo {
+
+    constructor(Mrn, Status, Reference, DateTime, Afzender, Ontvanger, Opdrachtgever,
+                AantalArtikelen, Totaalbedrag, Currency, TotaalGewicht){
+
+
+
+
+                    
+                }
+}
+
+
+
+
+
+
+
+
+
+// public class Declaration
+// {
+// 	public string Mrn;
+// 	public DeclarationStatus Status;
+// 	public string Reference;
+// 	public DateTime DateTime;	
+// 	public string Afzender;
+// 	public string Ontvanger;
+// 	public string Opdrachtgever;
+// 	public int AantalArtikelen;
+// 	public decimal TotaalBedrag;
+// 	public string Currency;
+// 	public decimal TotaalGewicht;
+// }
