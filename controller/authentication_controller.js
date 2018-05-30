5ca0c291847d7e31cc72088785e87340baeb27c0
Marco van Poortvliet <Marc0tjevp@gmail.com>
2018-05-30 12:55:02 +0200

    Register

--- controller/authentication_controller.js
+++ controller/authentication_controller.js
@@ -4,10 +4,10 @@ const db = require('../database/database')
 function login(req, res) {
 
     console.log('login function called')
-    //Undefined username/password, ff fixe
+
     var username = req.body.username || ''
     var password = req.body.password || ''
-    var imei =  req.body.imei || ''
+    var imei = req.body.imei || ''
 
     if (!password || !username) {
         res.json({
@@ -27,23 +27,12 @@ function login(req, res) {
 
         if (username == rows[0].username && password == rows[0].password && imei == rows[0].imei) {
             let token = auth.encodeToken(username)
-            console.log('Token: '+token + " entry: " + req.body.username + req.body.password + req.body.imei)
+            console.log('Token: ' + token + " entry: " + req.body.username + req.body.password + req.body.imei)
             res.status(200).json({
-                "message": token,
-                "status": 200,
-                "parameters": res.body
+                "token": token,
+                "status": 200
             })
         } else {
-            // if (!rows[0]) {
-            //     res.send("No valid credentials")
-            // } else if (username == rows[0].username && password == rows[0].password && imei == rows[0].imei) {
-            //     let token = auth.encodeToken(username)
-            //     response.status(200).json({
-            //         "message": token,
-            //         "status": 200,
-            //         "parameters": res.body
-            //     })
-            // }
             res.send('No valid credentials or imei is incorrect')
             console.log('Error login')
         }
@@ -54,42 +43,51 @@ function login(req, res) {
 
 function register(req, res) {
     console.log('register function called')
-    var username = req.body.username
-    var password = req.body.password
 
-    var query = {
-        sql: 'INSERT INTO `user`(username, password) VALUES (?, ?)', // deze query gaat problemen krijgen ivm auto increment en missende waardes zoals IMEI
-        values: [username, password],
+    var username = req.body.username || ''
+    var password = req.body.password || ''
+    var firstname = req.body.firstname || ''
+    var lastname = req.body.lastname || ''
+    var imei = req.body.imei || ''
+
+    var queryDriver = {
+        sql: 'INSERT INTO `driver`(firstname, lastname) VALUES (?,?)',
+        values: [firstname, lastname],
         timeout: 3000
     }
-    db.query('SELECT username FROM user WHERE username = ?', [username], function (error, result) {
-        if (result.length > 0) {
-            res.send('Email taken')
-            return
+
+    // Insert new driver first
+    db.query(queryDriver, function (error, result) {
+
+        if (error) {
+            res.json({
+                "msg": error
+            })
         } else {
-            if (username == '' || password == '') {
-                res.send('Missing properties')
-                return
+
+            // Get ID from just inserted driver
+            var resultID = result.insertId
+
+            var queryUser = {
+                sql: 'INSERT INTO `user`(username, password, driverID, imei) VALUES (?, ?, ?, ?)',
+                values: [username, password, resultID, imei],
+                timeout: 3000
             }
 
-            db.query(query, function (error, rows, field) {
-                if (error) {
-                    res.status(500).json({
-                        "message": error,
-                        "status": 500,
-                        "parameters": req.body
+            // Insert new user with driverID from previous query
+            db.query(queryUser, function (error, result) {
+                if (!error) {
+                    res.json({
+                        "msg": "registered new user"
                     })
                 } else {
-                    res.status(200).json({
-                        "message": "User has been registered",
-                        "status": 200,
-                        "parameters": req.body
+                    res.json({
+                        "msg": error
                     })
                 }
             })
         }
     })
-    console.log(username + " and " + password)
 }
 
 module.exports = {
