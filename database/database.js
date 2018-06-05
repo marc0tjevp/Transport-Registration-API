f71ba9f673d88d4fc3ecb03b0b0f7b759c894b72
Marco van Poortvliet <Marc0tjevp@gmail.com>
2018-06-05 13:14:35 +0200

    User credentials

--- database/database.js
+++ database/database.js
@@ -4,7 +4,7 @@ let config = require('./db_config')
 let connection = mysql.createConnection({
     host: process.env.DB_HOST || config.dbHost,
     user: process.env.DB_USER || config.dbUser,
-    password: 'secret', //process.env.DB_PASSWORD,
+    password: '', //process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE || config.dbDatabase
 })
 
