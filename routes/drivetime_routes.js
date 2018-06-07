5faee1c8ab9278f1095342479c30e1650e785de4
Marco van Poortvliet <Marc0tjevp@gmail.com>
2018-06-07 11:56:03 +0200

    Driver controller docs

--- routes/drivetime_routes.js
+++ routes/drivetime_routes.js
@@ -3,13 +3,32 @@ let controller = require('../controller/driver_controller')
 
 /**
  * Sends a block with drivetimes
- * @route GET /drivetimes/senddrive
+ * @route POST /drivetimes/senddrive
  * @group Drive Times
- * @return {object} 412 - Missing parameters, check if userID or imei is missing
- * @return {object} 200 - Edit succesfull
- * @returns {object}  500 - Unexpected error
+ * @param {string} starttime.required
+ * @param {string} endtime.required
+ * @param {string} travelTime.required
+ * @param {string} mrn.required
+ * @param {string} driverID.required
+ * @return {object} 412 - Please make sure to insert startTime,endTime, travelTime, mrn and driverID
+ * @return {object} 401 - Driver does not exist
+ * @return {object} 500 - SQL error occured
+ * @return {object} 200 - Successfully added drivetimes
+ * @return {object}  500 - Unexpected error
  */
 routes.post('/senddrive', controller.sendDriveTimes)
+
+/**
+ * Sends a block with drivetimes
+ * @route GET /drivetimes/senddrive
+ * @group Drive Times
+ * @param {string} driverID.required
+ * @return {object} 412 - Please make sure driverID is filled in
+ * @return {object} 401 - Drive times for this driverID does not exist
+ * @return {object} 500 - An error occured in the SQL query
+ * @return {object} 200 - {Object object}
+ * @return {object}  500 - Unexpected error
+ */
 routes.get('/getdrivebyid/:id', controller.getDriveTimeID)
 
 module.exports = routes
