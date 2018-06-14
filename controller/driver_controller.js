07662a43e6014b6b7d196e3665a38f89011187b2
Djim <oomesdjim@gmail.com>
2018-06-14 12:38:17 +0200

    fixed sendDriveTimes

--- controller/driver_controller.js
+++ controller/driver_controller.js
@@ -5,49 +5,60 @@ const ApiResponse = require('../model/ApiResponse')
 function sendDriveTimes(req, res) {
     console.log('POST sendDriveTimes function called')
 
+    var token = req.get('Authorization')
+    var subtoken = token.substr(7)
+	var decodedtoken = auth.decodeToken(subtoken)
+	var userID = decodedtoken.sub
+
     //Get params from body
     let startTime = req.body.startTime
     let endTime = req.body.endTime
     let type = req.body.type
     let mrn = req.body.mrn
-    let driverID = req.body.driverID
 
     //Check if params exist
-    if (!startTime || !endTime || !mrn || !driverID || !type) {
+    if (!startTime || !endTime || !mrn || !userID || !type) {
         res.status(412).json(new ApiResponse(412, "Please make sure to insert startTime, endTime, travelTime, mrn and driverID")).end()
         return
     }
 
     // Check if all paramaters are filled in
-    if (startTime == '' || endTime == '' || mrn == '' || driverID == '' || type == '') {
+    if (startTime == '' || endTime == '' || mrn == '' || userID == '' || type == '') {
         res.status(412).json(new ApiResponse(412, "Please make sure to insert startTime, endTime, travelTime, mrn and driverID")).end()
         return
     }
 
-    //Check if driver exists
-    db.query('SELECT * FROM driver WHERE driverID = ?', [driverID], function (error, rows, fields) {
+    db.query('SELECT * FROM driver WHERE userID = ?',[userID], function(err,row,field){
+        let driverID = row[0].driverID
+
+        //Check if driver exists
+        db.query('SELECT * FROM driver WHERE driverID = ?', [driverID], function (error, rows, fields) {
         if (!rows[0]) {
-            res.status(401).json(new ApiResponse(401, "Driver does not exist")).end()
-            return
+        res.status(401).json(new ApiResponse(401, "Driver does not exist")).end()
+        return
         } else {
 
-            // Execute select user query
-            let query = {
-                sql: 'INSERT INTO `drive_times`(startTime, endTime, type, mrn, driverID) VALUES (?,?,?,?,?)',
-                values: [startTime, endTime, type, mrn, driverID],
-                timeout: 3000
-            }
-            db.query(query, function (error, result) {
-
-                // Handle Mysql Errors
-                if (error) {
-                    res.status(500).json(new ApiResponse(500, error)).end()
-                } else {
-                    res.status(200).json(new ApiResponse(200, "Succesfully added drive times")).end()
-                }
-            })
+        // Execute select user query
+        let query = {
+            sql: 'INSERT INTO `drive_times`(startTime, endTime, type, mrn, driverID) VALUES (?,?,?,?,?)',
+            values: [startTime, endTime, type, mrn, driverID],
+            timeout: 3000
         }
+        db.query(query, function (error, result) {
+
+            // Handle Mysql Errors
+            if (error) {
+                res.status(500).json(new ApiResponse(500, error)).end()
+            } else {
+                res.status(200).json(new ApiResponse(200, "Succesfully added drive times")).end()
+            }
+        })
+    }
+})
+
     })
+
+    
 }
 
 function getDriveTimeID(req, res) {
