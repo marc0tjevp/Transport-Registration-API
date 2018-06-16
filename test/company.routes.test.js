0ca0117bb90685bef93996a0a46e974433807523
Djim <oomesdjim@gmail.com>
2018-06-16 13:27:40 +0200

    part two of start

--- test/company.routes.test.js
+++ test/company.routes.test.js
@@ -8,3 +8,20 @@ chai.use(chaiHttp)
 
 var validToken = "Bearer "+"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjI5fQ.tcdqhtUzjxcwQCnNHQYHuFet6V2eWdfbtIa60WH36Tc"
 
+describe('Company routes',()=>{
+    it('should return status 200 on succesfull register', (done) => {
+
+        chai.request(server)
+            .post('/company/driver/register')
+            .send({
+                "driverID": 31,
+                "mrn": "18IT123457999910PJ"
+            })
+            .set('Authorization', validToken)
+            .end(function (err, res) {
+                res.should.have.status(200)
+                done()
+            })       
+    })
+
+})
